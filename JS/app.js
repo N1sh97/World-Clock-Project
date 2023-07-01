0814f93257585d66ee53cde29d71c07c3b16e342
N1sh97 <112947529+N1sh97@users.noreply.github.com>
2023-07-01 17:03:12 +0100

    Created new id for each city to target in JS and then removed the fake data

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,20 @@
+setInterval(function () {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
 
+  parisDateElement.innerHTML = parisTime.format("MMM D0 YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
+setInterval(function () {
+  let hongKongElement = document.querySelector("#hong-kong");
+  let hongKongDateElement = hongKongElement.querySelector(".date");
+  let hongKongTimeElement = hongKongElement.querySelector(".time");
+  let hongKongTime = moment().tz("Asia/Hong_Kong");
+
+  hongKongDateElement.innerHTML = hongKongTime.format("MMM D0 YYYY");
+  hongKongTimeElement.innerHTML = hongKongTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
