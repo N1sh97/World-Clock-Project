a585b56dcc512727ccf5bd7cebb16695475ecbf6
N1sh97 <112947529+N1sh97@users.noreply.github.com>
2023-07-08 19:19:30 +0100

    updated js and added a link to redirect users to the main word clock once they select a new country

--- JS/app.js
+++ JS/app.js
@@ -50,7 +50,9 @@ function updateCity(event) {
         <div class="time">${cityTime.format(
           "h:mm:ss"
         )}<small> ${cityTime.format("A")}</small> </div>
-      </div>`;
+      </div>
+      br
+      <a href="/">All cities</a>`;
 }
 
 updateTime();
