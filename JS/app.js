d2b9f61f719e317db76069b6eab4f62391b8ad31
N1sh97 <112947529+N1sh97@users.noreply.github.com>
2023-07-09 13:41:56 +0100

    updated fuction name, to display city time zone names correctly

--- JS/app.js
+++ JS/app.js
@@ -44,7 +44,7 @@ function updateCity(event) {
   citiesElement.innerHTML = `
          <div class="city">
         <div>
-          <h2>${cityTimeZone}</h2>
+          <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format(
